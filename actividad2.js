d967e59d6444143d89d78317ee54f47932d13cb0
Xochitl Moreno <xochitl.mf44@gmail.com>
2024-04-14 16:07:31 -0600

    updated activity & included bonus

--- actividad2.js
+++ actividad2.js
@@ -3,10 +3,15 @@ peso = 150;
 altura = 1.76;
 
 let calcularIMC = peso / (altura * altura);
+//let calcularIMC = "dedo";
 
 //programa simple para que usuarios puedan calcular su IMC
 function IMC() {
     console.log("Tu IMC es de", calcularIMC);
+    //Bonus: Evalua el caso cuando el valor IMC calculado no sea válido (NaN), y muestra en consola un mensaje de error alertando que los valores iniciales no son válidos. 
+    if (isNaN(calcularIMC)) {
+        console.log("El numero ingresado no es valido")
+    }
 
     function resultados() {
         if (calcularIMC < 18.5) {
@@ -23,3 +28,5 @@ function IMC() {
 }
 
 IMC();
+
+
